7e16758351a5d5e8d5c83e1ada3bc1ba2e02e246
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-16 00:11:06 +0300

    add new user and tweet model

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+//const Tweet = require('./models/tweet')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('Server is up on the port ' + port)
+})
