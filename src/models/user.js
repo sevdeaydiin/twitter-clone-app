c3922fdc0a31bdb7248a446af2cc285f6bb3b484
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-16 01:12:26 +0300

    password hashing with bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -61,6 +62,28 @@ const userSchema = new mongoose.Schema({
         default: []
     }
 })
+
+// To delete password prior to GET
+userSchema.methods.toJson = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+
+    return userObject
+}
+
+// To hash the password
+userSchema.pre('save', async function(next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
