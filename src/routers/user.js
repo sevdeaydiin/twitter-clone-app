77bd5f017fc2c0f81464e6cc65e01045522ca001
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-16 00:55:48 +0300

    add user router

---
+++ src/routers/user.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const User = require('../models/user')
+
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    
+})
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.send(users)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+module.exports = router
