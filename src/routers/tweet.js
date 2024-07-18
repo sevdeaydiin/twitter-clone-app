211e33dc8116634be606b3bcb059007846483118
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-07-18 21:52:46 +0300

    tweet image upload

--- src/routers/tweet.js
+++ src/routers/tweet.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const Tweet = require('../models/tweet')
 const router = new express.Router()
 const auth = require('../middleware/auth')
+
+const upload = multer({
+    limits: {
+        fileSize: 100000000
+    }
+})
 
 // post tweet
 router.post('/tweets', auth, async (req, res) => {
@@ -18,14 +26,36 @@ router.post('/tweets', auth, async (req, res) => {
     }
 })
 
+// add image to tweet
+router.post('/uploadTweetImage/:id', auth, upload.single('upload'), async (req, res) => {
+
+    const tweet = await Tweet.findOne({ _id: req.params.id })
+    console.log(tweet)
+
+    if(!tweet) {
+        throw new Error('Cannot find the tweet')
+    }
+
+    const buffer = await sharp(req.file.buffer).resize({ width: 350, height: 350 }).png().toBuffer()
+    tweet.image = buffer
+    await tweet.save()
+    res.send()
+
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+// all tweet
 router.get('/tweets', async (req, res) => {
     try {
         const tweets = await Tweet.find({})
         res.send(tweets)
-        
+
     } catch(e) {
         res.status(500).send(e)
     }
 })
+
+
 
 module.exports = router
