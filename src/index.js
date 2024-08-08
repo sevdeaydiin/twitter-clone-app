8a7a857283b292ec6050545b60a89ed892cdfd77
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-08-08 11:28:50 +0300

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const express = require("express")
-//const Tweet = require('./models/tweet')
-
 const app = express()
 require('dotenv').config()
 var connectDB = require('./db/mongoose')
 connectDB()
+
 const userRouter = require('./routers/user')
 const tweetRouter = require('./routers/tweet')
+const notificationRouter = require('./routers/notification')
 app.use(express.json());
 app.use(express.static("public"));
+
 const port = process.env.PORT || 3000
 
 app.use(userRouter)
 app.use(tweetRouter)
+app.use(notificationRouter)
 
 app.listen(port, () => {
     console.log('Server is up on the port ' + port)
 })
-
